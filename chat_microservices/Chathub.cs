c4209949db74f19609bf6dd82befd244bf4ffd99
sebasruiz09 <sebasruiz909@gmail.com>
2022-05-17 05:31:21 +0200

    desarrollo de funcionalidad de chat como microservicio independiente'

---
+++ chat_microservices/Chathub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace chat_microservices
+{
+    public class Chathub : Hub
+    {
+        public async Task SendMessage(string room,string user , string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage", user , message);
+        }
+        public async Task AddToGroup(string room )
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, room);
+
+            await Clients.Group(room).SendAsync("ShowWho" ,
+                $"usuario conectado {Context.ConnectionId}");
+        }
+    }
+}
---
+++ chat_microservices/Controllers/ChatController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace chat_microservices.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, string> Rooms = 
+            new Dictionary<int, string>()
+        {
+                {1, "usuarios" }
+        };
+           
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room ( int room )
+        {
+            return View("Room", room);
+        }
+    }
+}
